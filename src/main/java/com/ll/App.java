bbede3a0dcb655f486f28c8354e03c2cdc31e880
i-am-jiwon <dlark1128@gmail.com>
2023-10-25 15:36:13 +0900

    step1

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 class App {
     void run() {
         System.out.println("=== 명언 앱 ===");
-        System.out.print("명령) ");
+        while(true) {
+            System.out.print("명령) ");
 
-        Scanner scanner = new Scanner(System.in);
-        String cmd = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
 
-        System.out.printf("입력하신 명령 : %s\n", cmd);
+            if( cmd.equals("종료")){
+                break;
+            }
 
+            System.out.printf("입력하신 명령 : %s\n", cmd);
+        }
     }
 }
